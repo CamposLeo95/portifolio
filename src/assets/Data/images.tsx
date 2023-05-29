a9ffab3f9ac64a917fab87c2416f9648e93707de
CamposLeo95 <leocampos.995@gmail.com>
2023-05-29 13:37:35 -0300

    build: includ portfolio imagem and remove others image

--- src/assets/Data/images.tsx
+++ src/assets/Data/images.tsx
@@ -2,7 +2,6 @@ import css from '../img/icons/css.png'
 import js from '../img/icons/js.png'
 import html from '../img/icons/html.png'
 import react from '../img/icons/react.png'
-import next from '../img/icons/next.png'
 import node from '../img/icons/node.png'
 import ts from '../img/icons/ts.png'
 import styled from '../img/icons/styled.png'
@@ -10,5 +9,5 @@ import git from '../img/icons/git.png'
 
 
 export const iconTec = {
-    css, js, html, react, next, node, ts, styled, git
+    css, js, html, react, node, ts, styled, git
 }
